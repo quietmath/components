ac5aec0f97347aaa70879c2230a8b74c6da91e57
Michael Szul <michael@szul.us>
2021-05-10 16:58:44 -0400

    Started building audio control

--- src/audio.ts
+++ src/audio.ts
@@ -2,8 +2,15 @@
  * @module quietmath/components
  */
 
-export const createAudioPlayer = (elementID: string): void => {
+export const createAudioPlayer = (elementID: string, defaultSource?: string): void => {
     const audio: HTMLAudioElement = document.querySelector(`${ elementID }`);
+    if(defaultSource != null) {
+        audio.src = defaultSource;
+    }
+    if(audio.controls == null) {
+        audio.controls = true;
+    }
+    audio.style.display = 'block';
 };
 
 export const createPlaylist = (elementID: string): void => {
