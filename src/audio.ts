161f984182dd88b64cd3d4d20da82b9cefe7d5c5
Michael Szul <michael@szul.us>
2021-05-14 17:08:06 -0400

    Adding playlist capabilities

--- src/audio.ts
+++ src/audio.ts
@@ -2,8 +2,8 @@
  * @module quietmath/components
  */
 
-export const createAudioPlayer = (elementID: string, defaultSource?: string): void => {
-    const audio: HTMLAudioElement = document.querySelector(`${ elementID }`);
+export const createAudioPlayer = (audioPlayerID: string, defaultSource?: string): void => {
+    const audio: HTMLAudioElement = document.querySelector(`${ audioPlayerID }`);
     if(defaultSource != null) {
         audio.src = defaultSource;
     }
@@ -13,6 +13,17 @@ export const createAudioPlayer = (elementID: string, defaultSource?: string): vo
     audio.style.display = 'block';
 };
 
-export const createPlaylist = (elementID: string): void => {
-    const playlistContainer: HTMLDivElement = document.querySelector(`${ elementID }`);
+export const createPlaylist = (audioPlayer: HTMLAudioElement, playlistContainerID: string, playlistID: string): void => {
+    const playlistContainer: HTMLDivElement = document.querySelector(`${ playlistContainerID }`);
+    const playlist: HTMLDataListElement = document.querySelector(`${ playlistID }`);
+    const anchorNodes: NodeListOf<HTMLElement> = playlist.querySelectorAll('dt');
+    const anchors = Array.from(anchorNodes);
+    anchors.forEach((anchor: HTMLDataListElement) => {
+        anchor.addEventListener('click', function(): void {
+            const source = anchor.getAttribute('data-source');
+            audioPlayer.pause();
+            audioPlayer.src = source;
+            audioPlayer.play();
+        });
+    });
 };
